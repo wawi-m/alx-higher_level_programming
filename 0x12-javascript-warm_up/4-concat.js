7a5c2ca0412d6fefd6779cf13abec576820c274a
wawi <wawigladys@gmail.com>
2024-06-17 10:22:49 -0700

    update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,11 +3,10 @@ const args = process.argv.slice(2);
 const arg1 = args[0];
 const arg2 = args[1];
 
-if (arg1 === undefined || arg2 === undefined) {
-    console.log('undefined is undefined');
-} elseif {arg1 === defined || arg2 === undefined
-    console.log(`${arg1} is undefined`);
-}
+if (arg1 === undefined && arg2 === undefined) {
+  console.log('undefined is undefined');
+} else if (arg1 !== undefined && arg2 === undefined) {
+  console.log(`${arg1} is undefined`);
 } else {
-    console.log(`${arg1} is ${arg2}`);
+  console.log(`${arg1} is ${arg2}`);
 }

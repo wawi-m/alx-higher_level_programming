0db1cb5d0a4d9818578f67a278daf56e64f4c133
wawi <wawigladys@gmail.com>
2024-06-17 09:50:48 -0700

    update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 
 if (args[0] === undefined) {
-    console.log("No argument");
+  console.log('No argument');
 } else {
-    console.log(args[0]);
+  console.log(args[0]);
 }

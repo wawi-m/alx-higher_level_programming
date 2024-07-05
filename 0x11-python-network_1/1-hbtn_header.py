a29267c39e866add578d6d04a78a484f94dbb4f6
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-07-06 00:40:51 +0300

    Update file 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,11 +2,31 @@
 import urllib.request
 import sys
 
+def fetch_x_request_id(url):
+    """
+    Fetches the value of the X-Request-Id variable from the header of the response
+    of the given URL.
+
+    Args:
+    - url (str): The URL to send the request to.
+
+    Prints:
+    - The value of the X-Request-Id variable found in the header of the response.
+    """
+    try:
+        with urllib.request.urlopen(url) as response:
+            x_request_id = response.getheader('X-Request-Id')
+            if x_request_id:
+                print(x_request_id)
+            else:
+                print("No X-Request-Id found in the response headers.")
+    except urllib.error.URLError as e:
+        print(f"Error fetching URL: {e}")
+
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io'
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
 
-    with urllib.request.urlopen(url) as response:
-        headers = response.getheaders()
-        x_request_id = dict(headers).get('X-Request-Id')
-
-    print(x_request_id)
+    url = sys.argv[1]
+    fetch_x_request_id(url)

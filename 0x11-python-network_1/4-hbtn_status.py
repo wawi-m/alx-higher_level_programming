65d9baf791e03f24cfd710dbefc3750e7e15bbcc
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-07-05 23:27:56 +0300

    Update file 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
+"""
+This module fetches the status from https://alx-intranet.hbtn.io/status
+using the requests package and displays the body of the response.
+"""
+
 import requests
 
-url = 'https://alx-intranet.hbtn.io/status'
+def fetch_status():
+    """
+    Fetches the status from the given URL and prints the response
+    in a formatted manner.
+    """
+    url = 'https://alx-intranet.hbtn.io/status'
+    
+    response = requests.get(url)
+    body = response.text
+    
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body))
 
-response = requests.get(url)
-body = response.text
-
-print("Body response:")
-print("\t- type: {}".format(type(body)))
-print("\t- content: {}".format(body))
-print("\t- utf8 content: {}".format(body))
+if __name__ == "__main__":
+    fetch_status()

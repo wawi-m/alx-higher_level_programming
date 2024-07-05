49108686d16913ae1e58b6a2618fe318fa1ec036
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-07-05 23:05:19 +0300

    Create file 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")

691c059b2977bb9175f878edd701b6040e78ab13
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-07-05 23:06:41 +0300

    Create 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+import requests
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+response = requests.get(url)
+body = response.text
+
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body))

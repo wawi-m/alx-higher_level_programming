8cf33aea8152ff44ba151911cb54ef5c1252ddc5
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-07-06 00:43:01 +0300

    Update file 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,7 @@
 import urllib.request
 import sys
 
+
 def fetch_x_request_id(url):
     """
     Fetches the value of the X-Request-Id variable from the header of the response
@@ -22,6 +23,7 @@ def fetch_x_request_id(url):
                 print("No X-Request-Id found in the response headers.")
     except urllib.error.URLError as e:
         print(f"Error fetching URL: {e}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:

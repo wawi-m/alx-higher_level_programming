6d6f22e5b160b1fa19ebb1428d42bd9ad29e980f
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-07-06 00:49:22 +0300

    Create file 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python3
+import requests
+import sys
 
+def fetch_x_request_id(url):
+
+    try:
+        response = requests.get(url)
+        x_request_id = response.headers.get('X-Request-Id')
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("No X-Request-Id found in the response headers.")
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching URL: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    fetch_x_request_id(url)

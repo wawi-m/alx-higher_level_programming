3c2dc6d4e49d3823a8f81ea42d5ca8c5ffc842ad
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-07-05 23:04:05 +0300

    Create file 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    with urllib.request.urlopen(url, data=data) as response:
+        body = response.read()
+        print(body.decode('utf-8'))

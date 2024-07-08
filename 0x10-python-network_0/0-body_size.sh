4b3d5d2c8a282adbcada48000dc07dda6ca8a010
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-07-08 19:22:10 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl body size
-echo "Size of the body of the response: $(curl -s -o /dev/null -w '%{size_download}' "$1") bytes"
+echo " $(curl -s -o /dev/null -w '%{size_download}' "$1")"

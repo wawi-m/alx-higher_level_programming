56350023812824916ee264911a17c9622ec41836
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-07-08 19:38:31 +0300

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the body of the response
-[ "$(curl -s -o -I -w '%{http_code}' "$1")" -eq 200 ] && curl -s "$1"
+BODY=$(curl -Ls -w "%{http_code}" "$1" -o temp_body); [ "$BODY" -eq 200 ] && cat temp_body; rm -f temp_body

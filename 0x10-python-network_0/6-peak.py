7c21e93ad39509006e813f0949c02971a382bf8a
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-07-15 13:47:44 +0000

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" finds a peak in a list of unsorted integers"""
+"""lowest complexity algorithm for list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
     if list_of_integers:
         list_of_integers.sort(reverse=True)
         return list_of_integers[0]

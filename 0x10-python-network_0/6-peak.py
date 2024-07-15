b8e95417f6b5acda76ddc0129a1d94cb6c9589f6
wawi <wawigladys@gmail.com>
2024-07-15 06:27:33 -0700

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" finds a peak in a list of unsorted integers"""
+
+def find_peak(list_of_integers):
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
